8f979cec73115958495248a4c57126fb5885ab52
Tema-front <polsowatel.artem@gmail.com>
2022-02-19 15:36:02 +0300

    Updates

--- src/store/filter/selectors.js
+++
@@ -1,1 +0,0 @@
-export const getFilters = (state) => state.stateFilter.filters
--- src/store/profile/selectors.js
+++
@@ -1,2 +0,0 @@
-export const getAuthed = (state) => state.stateProfile.authed
-export const getName = (state) => state.stateProfile.name
